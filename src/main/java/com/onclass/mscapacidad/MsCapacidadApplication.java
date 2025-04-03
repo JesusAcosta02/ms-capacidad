1550334222995293d62ce630af75f2ddfe8873b2
JesusPersonal <jeacoh02@gmail.com>
2025-04-03 08:39:12 -0500

    Initial commit

---
+++ src/main/java/com/onclass/mscapacidad/MsCapacidadApplication.java
@@ -0,0 +1,13 @@
+package com.onclass.mscapacidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCapacidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCapacidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onclass/mscapacidad/MsCapacidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onclass.mscapacidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCapacidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
